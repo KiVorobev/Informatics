469946cba686cf43381c23b5717c58655bb175fa
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2021-09-26 15:58:07 +0300

    Create lab_03_02.py

---
+++ Lab6/lab_03_02.py
@@ -0,0 +1,22 @@
+'''
+ Множества
+'''
+# создание множества
+b1 = set()
+print("Set b1 = ", b1)
+b2 = {"bear", "fox", "squirrel", "woodpecker",
+"woodpecker", "wolf", "hedgehog"}
+print("Set b2 = ", b2)
+# создание множества из строки
+b3 = set("abcdabcdefg")
+print("Set b3 from string: ", set(b3))
+print("\n")
+
+
+s = "Electricity is the set of physical phenomena associated with the presence of electric charge. Lightning is one of the most dramatic effects of electricity"
+set1 = set(s)
+print(set1)
+vowels = ('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y')
+for i in range(len(vowels)):
+    if vowels[i] in set1:
+        print(vowels[i], end= ' ')
