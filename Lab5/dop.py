cf23167281c23d5c9199d683fc44558dfecc1ea9
KiVorobev <79376783+KiVorobev@users.noreply.github.com>
2022-03-15 14:54:43 +0300

    test del

--- Lab5/dop.py
+++
@@ -1,8 +0,0 @@
-def shortener(st):
-    while '(' in st or ')' in st:
-        a = st.rfind('(')
-        b = st.find(')', a)
-        st = st.replace(st[a:b + 1], '')
-    return st
-st = str(input())
-print(shortener(st))
